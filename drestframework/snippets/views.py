f1a9e14dd9b2a61e31cf69d5be4f6f2a50cd7a7a
raphfso <raphael.oliveira@brilliantmachine.com.br>
2018-05-23 00:29:54 -0300

    imports

--- drestframework/snippets/views.py
+++ drestframework/snippets/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
 # Create your views here.
