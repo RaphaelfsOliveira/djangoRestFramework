c2a4964b741e5855707e5af426e5c132fe02c000
raphfso <raphael.oliveira@brilliantmachine.com.br>
2018-05-22 22:51:47 -0300

    create serializers

--- drestframework/snippets/serializers.py
+++ drestframework/snippets/serializers.py
@@ -6,11 +6,26 @@ class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
     def create(self, validate_data):
         """
         Criar e retornar uma nova instancia de 'Snippet', recebendo dados validos
         """
-        
+        return Snippet.objects.create(**validate_data)
+
+    def update(self, instance, validate_data):
+        """
+        Atualizar e retornar uma instancia de Snippet, recebendo dados validos
+        """
+        instance.title = validate_data.get('title', instance.title)
+        instance.code = validate_data.get('code', instance.code)
+        instance.linenos = validate_data.get('linenos', instance.linenos)
+        instance.language = validate_data.get('language', instance.language)
+        instance.style = validate_data.get('style', instance.style)
+        instance.save()
+        return instance
+
+    
