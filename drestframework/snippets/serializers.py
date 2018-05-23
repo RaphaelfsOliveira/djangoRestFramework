aba3b7a2dfd545ac3bd6c5bfdeda0f4ae94ebec8
raphfso <raphael.oliveira@brilliantmachine.com.br>
2018-05-22 22:01:02 -0300

    serializers

---
+++ drestframework/snippets/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validate_data):
+        """
+        Criar e retornar uma nova instancia de 'Snippet', recebendo dados validos
+        """
+        
