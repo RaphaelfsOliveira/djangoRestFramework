a4eb600b28880a7b9035aec1e68b95188e00d1a1
raphfso <raphael.oliveira@brilliantmachine.com.br>
2018-05-23 00:29:37 -0300

    space patterns

--- drestframework/snippets/serializers.py
+++ drestframework/snippets/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
