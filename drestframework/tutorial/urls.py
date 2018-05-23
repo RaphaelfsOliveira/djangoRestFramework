6972684c878f418a818a7fcbb09ea218cf7bb345
raphfso <raphael.oliveira@brilliantmachine.com.br>
2018-05-23 01:36:18 -0300

    urls

--- drestframework/tutorial/urls.py
+++ drestframework/tutorial/urls.py
@@ -24,7 +24,8 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^', include('snippets.urls', namespace='snippets')),
+    url(r'^api-user/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
