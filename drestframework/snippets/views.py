ca159cbf1f4e76ee5a21b5e206e419d2467af172
raphfso <raphael.oliveira@brilliantmachine.com.br>
2018-06-02 12:30:54 -0300

    refactoring snippet_detail

--- drestframework/snippets/views.py
+++ drestframework/snippets/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
+
 # Create your views here.
 @api_view(['GET', 'POST'])
 def snippet_list(request):
@@ -24,27 +25,28 @@ def snippet_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def snippet_detail(request, pk):
     """
-    Atualiza ou deleta um code snippet
+    Retorna somente um, Atualiza ou deleta um code snippet
     """
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data, status=200)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=200)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
