e7bc4d06a01c1ba53fd7bc0944c117c2c3ba0eb4
raphfso <raphael.oliveira@brilliantmachine.com.br>
2018-06-02 12:11:53 -0300

    refactoring get and post with drf functions

--- drestframework/snippets/views.py
+++ drestframework/snippets/views.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 # Create your views here.
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def snippet_list(request):
     """
     Listar todos os code snippets, ou criar um novo snippet
@@ -17,17 +15,15 @@ def snippet_list(request):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, status=200, safe=False)
+        return Response(serializer.data)  # status 200 ok com return automatico
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
 def snippet_detail(request, pk):
     """
     Atualiza ou deleta um code snippet
